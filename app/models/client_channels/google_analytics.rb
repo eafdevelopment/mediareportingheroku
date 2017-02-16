520ef06b364593658bc02af9a147eacf75f25b4a
imarkwick <izzymarkwick@gmail.com>
2017-02-16 16:28:36 +0000

    temp google analytics patch

--- app/models/client_channels/google_analytics.rb
+++ app/models/client_channels/google_analytics.rb
@@ -15,37 +15,37 @@ class ClientChannels::GoogleAnalytics < ClientChannel
     rr.metrics = [metric("ga:sessions"), metric("ga:sessionDuration")]
     rr.date_ranges = [dateRange("7daysAgo", "today")]
     grr.report_requests = [rr]
-    response = client.batch_get_reports(grr)
-    response.reports.each do |report|
-      report.data.rows.each do |row|
-        puts row
-      end
-    end
+    # response = client.batch_get_reports(grr)
+    # response.reports.each do |report|
+    #   report.data.rows.each do |row|
+    #     puts row
+    #   end
+    # end
   end
 
   private
 
-  def client
-    creds = ServiceAccountCredentials.make_creds({
-      :json_key_io => File.open(Rails.root.join('client_secrets_4023d7d9e162.json').to_s),
-      :scope => 'https://www.googleapis.com/auth/analytics.readonly'
-    })
-    client = AnalyticsReportingService.new
-    client.authorization = creds
-    return client
-  end
+  # def client
+  #   creds = ServiceAccountCredentials.make_creds({
+  #     :json_key_io => File.open(Rails.root.join('client_secrets_4023d7d9e162.json').to_s),
+  #     :scope => 'https://www.googleapis.com/auth/analytics.readonly'
+  #   })
+  #   client = AnalyticsReportingService.new
+  #   client.authorization = creds
+  #   return client
+  # end
 
-  def dateRange(startDate, endDate)
-    range = DateRange.new
-    range.start_date = startDate
-    range.end_date = endDate
-    return range
-  end
+  # def dateRange(startDate, endDate)
+  #   range = DateRange.new
+  #   range.start_date = startDate
+  #   range.end_date = endDate
+  #   return range
+  # end
 
-  def metric(expression)
-    metric = Metric.new
-    metric.expression = expression
-    return metric
-  end
+  # def metric(expression)
+  #   metric = Metric.new
+  #   metric.expression = expression
+  #   return metric
+  # end
 
 end
