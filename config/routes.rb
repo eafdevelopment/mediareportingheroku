bb7b6633c9c8b69ae8e45674889560ad94970f63
imarkwick <izzymarkwick@gmail.com>
2017-02-16 13:52:56 +0000

    adding clients and campaign controllers and form for new campaign

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   resources :home, only: :index
   resources :reports, only: :index
+  resources :clients, only: [:index, :show]
+  resources :campaigns, only: [:new, :create]
 
   get 'home/update_campaigns', as: 'update_campaigns'
 
