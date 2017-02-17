3c2c01adc58041fe65e5bfc2f0f14d79e33fa7f9
Louise Swift <louise@louiseswift.com>
2017-02-17 16:14:04 +0000

    Remove debug messages

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,8 +9,6 @@ class ReportsController < ApplicationController
     @results = []
     client_channels = @client.client_channels.where(type: params[:channel])
     campaign_channels = @campaign.campaign_channels.where(client_channel_id: client_channels.map(&:id))
-    puts "\n> client_channels: " + client_channels.inspect
-    puts "\n> campaign_channels: " + campaign_channels.inspect
     campaign_channels.each do |campaign_channel|
       @results.push(
         campaign_channel.client_channel.fetch_metrics({
@@ -19,13 +17,6 @@ class ReportsController < ApplicationController
           startDate: params[:date_to]
         })
       )
-    end
-    puts "\n> @results: " + @results.inspect
-    @results.each do |thing|
-      puts ">> thing: " + thing.inspect
-      # thing.each do |thing2|
-      #   puts ">>> thing2: " + thing2.inspect
-      # end
     end
   end
 
