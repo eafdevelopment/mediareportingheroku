84bfa25b41c66749c2ac1d7e43055f16bdfffa3d
imarkwick <izzymarkwick@gmail.com>
2017-02-17 18:27:42 +0000

    adding basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_filter :basic_auth
+
+  def basic_auth
+    if Rails.env.production?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == "eight&four" && password == "welovemayo"
+      end
+    end
+  end
 end
