8ae4da16030a2aa0520796b27724456e7e01f103
Louise Swift <louise@louiseswift.com>
2017-02-20 16:55:56 +0000

    Tidy up old commented-out methods

--- app/models/client_channels/facebook.rb
+++ app/models/client_channels/facebook.rb
@@ -1,18 +1,5 @@
 class ClientChannels::Facebook < ClientChannel
 
-  # def fetch_campaigns(client)
-  #   # Find and store campaigns from a Facebook account in our database
-  #   FacebookAds.access_token = ENV["FACEBOOK_ACCESS_TOKEN"]
-  #   account = FacebookAds::AdAccount.find("act_1406179109678952")
-  #   account_campaigns = account.ad_campaigns #(effective_status: nil)
-  #   account_campaigns.each do |fb_campaign|
-  #     campaign = Campaign.find_or_create_by(name: fb_campaign.name)
-  #     campaign.client = Client.find_by(id: client.id)
-  #     campaign.facebook_campaign_id = fb_campaign.id
-  #     campaign.save!
-  #   end
-  # end
-
   def fetch_metrics(from_date, to_date, uid, optional={})
     # Find and return Insights for a Facebook account & campaign
     # TEMPORARILY SWITCHING TO DUMMY DATA
