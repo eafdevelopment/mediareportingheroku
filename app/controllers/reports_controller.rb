66a499f62353485f008450452e2d8d3631d4f8a2
Louise Swift <louise@louiseswift.com>
2017-02-20 15:13:18 +0000

    Basic report date range validation

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,10 @@
 class ReportsController < ApplicationController
 
   def index
+    unless valid_date_range
+      flash[:alert] = "Invalid date range."
+      redirect_back(fallback_location: root_path) and return
+    end
     @client = Client.find(params[:client])
     @campaign = Campaign.find(params[:campaign])
     # we should receive an array of client_channels so we could perhaps loop through
@@ -17,6 +21,18 @@ class ReportsController < ApplicationController
           from_date: params[:date_to]
         })
       )
+    end
+  end
+
+  private
+
+  def valid_date_range
+    if params[:date_from].present? && params[:date_to].present?
+      from = Date.parse(params[:date_from])
+      to = Date.parse(params[:date_to])
+      if from <= to
+        return true
+      end
     end
   end
 
