d94bee26cf4400266aecd0a93f34eecdbdfd9fb7
Louise Swift <louise@louiseswift.com>
2017-02-20 15:53:26 +0000

    Include a DMU Int'l Facebook campaign in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,40 +2,38 @@
 dmu_1 = Client.find_or_create_by(name: "DMU Leicester Castle Business School")
 
 # DMU Leicester Castle on Facebook
-dmu_1.client_channels.create({
+dmu_1.client_channels.where(
   type: "ClientChannels::Facebook",
   uid: "act_1376049629358567"
-})
-
-# DMU Leicester Castle on Google Analytics
-# dmu_1.client_channels.create({
-#   type: "ClientChannels::GoogleAnalytics",
-#   uid: ""
-# }) # ! we may not be using Google Analytics like this
+).first_or_create
 
 # DMU Leicester Castle on Twitter
-dmu_1.client_channels.create({
+dmu_1.client_channels.where(
   type: "ClientChannels::Twitter",
   uid: "hello_twitter_1"
-})
+).first_or_create
 
 # Create DMU International
 dmu_2 = Client.find_or_create_by(name: "DMU International")
 
 # DMU International on Facebook
-dmu_2.client_channels.create!({
+dmu_2.client_channels.where(
   type: "ClientChannels::Facebook",
   uid: "act_1406179109678952"
-})
-
-# DMU International on Google Analytics
-# dmu_2.client_channels.create({
-#   type: "ClientChannels::GoogleAnalytics",
-#   uid: "110182207"
-# }) # ! we may not be using Google Analytics like this
+).first_or_create
 
 # DMU International on Twitter
-dmu_2.client_channels.create!({
+dmu_2.client_channels.where(
   type: "ClientChannels::Twitter",
   uid: "hello_twitter_2"
-})
+).first_or_create
+
+# A DMU International campaign
+dmu_2_campaign = dmu_2.campaigns.find_or_create_by(name: "An Example Campaign for DMU International")
+
+# This campaign on DMU's Facebook
+CampaignChannel.where(
+  campaign_id: dmu_2_campaign.id,
+  client_channel_id: dmu_2.client_channels.where(type: ClientChannels::Facebook).first.id,
+  uid: "6063884630694"
+).first_or_create
