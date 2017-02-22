4ef7162457cdfde63f88f0676ae899a664dc325b
Louise Swift <louise@louiseswift.com>
2017-02-22 17:45:45 +0000

    New & Edit Client forms and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,13 @@ Rails.application.routes.draw do
 
   resources :home, only: :index
   resources :reports, only: :index
-  resources :clients, only: [:index, :show] do
+  resources :clients do
     resources :campaigns, only: [:index, :new, :create, :edit, :destroy, :update] do
     end
   end
   resources :campaigns, only: [] do
     resources :campaign_channels, only: [:index]
   end
-  
-  # get 'home/update_campaigns', as: 'update_campaigns'
-  # get 'home/update_campaign_channels', as: 'update_campaign_channels'
 
   root to: 'home#index'
 end
