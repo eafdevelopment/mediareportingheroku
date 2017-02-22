7614854372d3fd4f5ffbd0eb78d7d994af41ad0b
Louise Swift <louise@louiseswift.com>
2017-02-22 17:22:12 +0000

    Rewrite seeds.db to contain only DMU Intl, DMU EU & Dirty Martini Facebook campaigns

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,89 +1,137 @@
-# Create DMU Leicester Castle
-dmu_1 = Client.where(
-  name: "DMU - Leicester Castle Business School",
-  google_analytics_view_id: ""
-).first_or_create
-
 # Create DMU International
-dmu_2 = Client.where(
+dmu_intl = Client.where(
   name: "DMU International - eight&four",
-  google_analytics_view_id: ""
-).first_or_create
-
-# Create De Montfort University Leicester (DMU)
-dmu_3 = Client.where(
-  name: "De Montfort University Leicester (DMU)",
-  google_analytics_view_id: ""
-).first_or_create
-
-# DMU Leicester Castle on Facebook
-dmu_1.client_channels.where(
-  type: "ClientChannels::Facebook",
-  uid: "act_1143706438973923"
+  google_analytics_view_id: "110182207"
 ).first_or_create
 
 # DMU International on Facebook
-dmu_2.client_channels.where(
+dmu_intl.client_channels.where(
   type: "ClientChannels::Facebook",
   uid: "act_1406179109678952"
 ).first_or_create
 
-# De Montfort University on Facebook
-dmu_3.client_channels.where(
+# Create DMU (EU)
+dmu_eu = Client.where(
+  name: "DMU (EU)",
+  google_analytics_view_id: ""
+).first_or_create
+
+# DMU (EU) on Facebook
+dmu_eu.client_channels.where(
   type: "ClientChannels::Facebook",
-  uid: "act_1376049629358567"
+  uid: "act_1406179109678952" # Same as DMU International
 ).first_or_create
 
-# De Montfort University campaigns
-dmu_3_campaign_1 = dmu_3.campaigns.find_or_create_by(name: "recruitment_2017_facebook_acquisition_traffic_driving_video")
-dmu_3_campaign_2 = dmu_3.campaigns.find_or_create_by(name: "2017_interview_remarketing_promoted_posts")
-dmu_3_campaign_3 = dmu_3.campaigns.find_or_create_by(name: "conversion_remarketing_2017_instagram_traffic_driving")
-dmu_3_campaign_4 = dmu_3.campaigns.find_or_create_by(name: "conversion_remarketing_2017_facebook_promoted_posts")
-dmu_3_campaign_5 = dmu_3.campaigns.find_or_create_by(name: "recruitment_2017_facebook_acquisition_canvas_traffic_driving")
-dmu_3_campaign_6 = dmu_3.campaigns.find_or_create_by(name: "recruitment_2017_facebook_acquisition_traffic_driving_image")
-dmu_3_campaign_7 = dmu_3.campaigns.find_or_create_by(name: "Promoted Pages - Audience Generation")
+# DMU International campaigns
+dmu_intl_campaign_1 = dmu_intl.campaigns.find_or_create_by(name: "offer_holders_sept_2017_promoted_post_remarketing")
+dmu_intl_campaign_2 = dmu_intl.campaigns.find_or_create_by(name: "sept_2017_promoted_post_remarketing")
+dmu_intl_campaign_3 = dmu_intl.campaigns.find_or_create_by(name: "all_markets_sept_2017_enquiry_remarketing_promoted_posts")
+dmu_intl_campaign_4 = dmu_intl.campaigns.find_or_create_by(name: "egypt_sept_2017_recruitment_traffic driving")
+dmu_intl_campaign_5 = dmu_intl.campaigns.find_or_create_by(name: "nigeria_sept_2017_recruitment_traffic driving")
+dmu_intl_campaign_6 = dmu_intl.campaigns.find_or_create_by(name: "india_sept_2017_recruitment_traffic driving")
+dmu_intl_campaign_7 = dmu_intl.campaigns.find_or_create_by(name: "pakistan_sept_2017_recruitment_traffic driving")
+dmu_intl_campaign_8 = dmu_intl.campaigns.find_or_create_by(name: "thailand_sept_2017_recruitment_traffic driving")
 
-# These campaigns on De Montfort University's Facebook
+# These campaigns on Facebook
 CampaignChannel.where(
-  campaign_id: dmu_3_campaign_1.id,
-  client_channel_id: dmu_3.client_channels.find_by(type: ClientChannels::Facebook).id,
-  uid: "6066496188078",
+  campaign_id: dmu_intl_campaign_1.id,
+  client_channel_id: dmu_intl.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6063884630694",
   google_analytics_campaign_name: ""
 ).first_or_create
 CampaignChannel.where(
-  campaign_id: dmu_3_campaign_2.id,
-  client_channel_id: dmu_3.client_channels.find_by(type: ClientChannels::Facebook).id,
-  uid: "6065738876478",
+  campaign_id: dmu_intl_campaign_2.id,
+  client_channel_id: dmu_intl.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6063884618094",
   google_analytics_campaign_name: ""
 ).first_or_create
 CampaignChannel.where(
-  campaign_id: dmu_3_campaign_3.id,
-  client_channel_id: dmu_3.client_channels.find_by(type: ClientChannels::Facebook).id,
-  uid: "6063185312278",
+  campaign_id: dmu_intl_campaign_3.id,
+  client_channel_id: dmu_intl.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6063875073494",
   google_analytics_campaign_name: ""
 ).first_or_create
 CampaignChannel.where(
-  campaign_id: dmu_3_campaign_4.id,
-  client_channel_id: dmu_3.client_channels.find_by(type: ClientChannels::Facebook).id,
-  uid: "6063176740078",
+  campaign_id: dmu_intl_campaign_4.id,
+  client_channel_id: dmu_intl.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6063533917094",
   google_analytics_campaign_name: ""
 ).first_or_create
 CampaignChannel.where(
-  campaign_id: dmu_3_campaign_5.id,
-  client_channel_id: dmu_3.client_channels.find_by(type: ClientChannels::Facebook).id,
-  uid: "6061533889878",
+  campaign_id: dmu_intl_campaign_5.id,
+  client_channel_id: dmu_intl.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6063533821494",
   google_analytics_campaign_name: ""
 ).first_or_create
 CampaignChannel.where(
-  campaign_id: dmu_3_campaign_6.id,
-  client_channel_id: dmu_3.client_channels.find_by(type: ClientChannels::Facebook).id,
-  uid: "6056859080478",
+  campaign_id: dmu_intl_campaign_6.id,
+  client_channel_id: dmu_intl.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6063533797494",
   google_analytics_campaign_name: ""
 ).first_or_create
 CampaignChannel.where(
-  campaign_id: dmu_3_campaign_7.id,
-  client_channel_id: dmu_3.client_channels.find_by(type: ClientChannels::Facebook).id,
-  uid: "6028768937278",
+  campaign_id: dmu_intl_campaign_7.id,
+  client_channel_id: dmu_intl.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6063533863094",
+  google_analytics_campaign_name: ""
+).first_or_create
+CampaignChannel.where(
+  campaign_id: dmu_intl_campaign_8.id,
+  client_channel_id: dmu_intl.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6063533367694",
+  google_analytics_campaign_name: ""
+).first_or_create
+
+# DMU (EU) campaigns
+dmu_eu_campaign_1 = dmu_eu.campaigns.find_or_create_by(name: "uk_domicile_sept_2017_recruitment_traffic driving")
+dmu_eu_campaign_2 = dmu_eu.campaigns.find_or_create_by(name: "romania_eu_2017_recruitment_traffic driving")
+dmu_eu_campaign_3 = dmu_eu.campaigns.find_or_create_by(name: "greece_eu_2017_recruitment_traffic driving")
+dmu_eu_campaign_4 = dmu_eu.campaigns.find_or_create_by(name: "bulgaria_eu_2017_recruitment_traffic driving")
+dmu_eu_campaign_5 = dmu_eu.campaigns.find_or_create_by(name: "poland_eu_2017_recruitment_traffic driving")
+dmu_eu_campaign_6 = dmu_eu.campaigns.find_or_create_by(name: "lithuania_eu_2017_recruitment_traffic driving")
+dmu_eu_campaign_7 = dmu_eu.campaigns.find_or_create_by(name: "cyprus_eu_2017_recruitment_traffic driving")
+
+# These campaigns on Facebook
+CampaignChannel.where(
+  campaign_id: dmu_eu_campaign_1.id,
+  client_channel_id: dmu_eu.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6063533895894",
+  google_analytics_campaign_name: ""
+).first_or_create
+CampaignChannel.where(
+  campaign_id: dmu_eu_campaign_2.id,
+  client_channel_id: dmu_eu.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6062746812894",
+  google_analytics_campaign_name: ""
+).first_or_create
+CampaignChannel.where(
+  campaign_id: dmu_eu_campaign_3.id,
+  client_channel_id: dmu_eu.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6062746812294",
+  google_analytics_campaign_name: ""
+).first_or_create
+CampaignChannel.where(
+  campaign_id: dmu_eu_campaign_4.id,
+  client_channel_id: dmu_eu.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6062746793294",
+  google_analytics_campaign_name: ""
+).first_or_create
+CampaignChannel.where(
+  campaign_id: dmu_eu_campaign_5.id,
+  client_channel_id: dmu_eu.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6062746792894",
+  google_analytics_campaign_name: ""
+).first_or_create
+CampaignChannel.where(
+  campaign_id: dmu_eu_campaign_6.id,
+  client_channel_id: dmu_eu.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6061752368094",
+  google_analytics_campaign_name: ""
+).first_or_create
+CampaignChannel.where(
+  campaign_id: dmu_eu_campaign_7.id,
+  client_channel_id: dmu_eu.client_channels.find_by(type: ClientChannels::Facebook).id,
+  uid: "6061748394694",
   google_analytics_campaign_name: ""
 ).first_or_create
 
