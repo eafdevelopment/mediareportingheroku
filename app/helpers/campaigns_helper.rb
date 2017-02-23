a3517b40d358cccf9dd8f48976ff2d466f0be48a
Louise Swift <louise@louiseswift.com>
2017-02-23 12:53:49 +0000

    Adjust New Campaign form fields

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,11 +1,2 @@
 module CampaignsHelper
-
-  def uid_requirement(client_channel)
-    channel = name(client_channel)
-    if channel == 'Facebook'
-      'Advert Set ID'
-    else
-      'Unique ID'
-    end
-  end
 end
