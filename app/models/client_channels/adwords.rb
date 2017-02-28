911b8943ed83414ec421313988b6dc23ea596566
David Somers <david@jalada.co.uk>
2017-02-28 12:45:45 +0000

    Remove unnecessary auth hash options, fix structure of token in config

--- app/models/client_channels/adwords.rb
+++ app/models/client_channels/adwords.rb
@@ -22,27 +22,14 @@ class ClientChannels::Adwords < ClientChannel
     # whatever OAuth2 client_id you're using in the Developer Console)
     # https://accounts.google.com/o/oauth2/auth?response_type=code&client_id=805050819915-8or80ct5vsanmfi0mt1mkhkus04pncii.apps.googleusercontent.com&redirect_uri=http://localhost:3000&scope=https://www.googleapis.com/auth/adwords&access_type=offline
     # END TEMP
+    
+    authentication_hash = AppConfig.adwords.merge({
+      user_agent: 'eight&four',
+      client_customer_id: '316-190-9175'
+    })
+
     adwords = AdwordsApi::Api.new({
-      :authentication => {
-          :access_token => ENV['ADWORDS_ACCESS_TOKEN'],
-          # :method => 'OAUTH2_SERVICE_ACCOUNT',
-          # :oauth2_issuer => 'marketing-reporting@abiding-arch-104914.iam.gserviceaccount.com',
-          # :oauth2_keyfile => Rails.root.join("private", "analytics_client_secret.json").to_s,
-          :method => 'OAuth2',
-          # :oauth2_key => ENV['GOOGLE_CLIENT_SECRETS'],
-          # :oauth2_client_id => '805050819915-4oa7i4v3c1mois8rd1f3e4389cq0acl8.apps.googleusercontent.com',
-          # :oauth2_client_secret => 'KEPuO1x693iP4bW8_jF-PcOz',
-          # :refresh_token => '1/xRj-7WXXpVPTD4k7ss8m73Ivph4zFudEznn_StRVb7M',
-          :developer_token => 'VLRWrvozHMMogpI6rJdsBQ',
-          :user_agent => 'eight&four',
-          :client_customer_id => '316-190-9175', #'529-163-1130',
-          :oauth2_client_id => ENV['ADWORDS_OAUTH2_CLIENT_ID'],
-          :oauth2_client_secret => ENV['ADWORDS_OAUTH2_CLIENT_SECRET'],
-          :refresh_token => ENV['ADWORDS_REFRESH_TOKEN'],
-          :developer_token => ENV['ADWORDS_DEVELOPER_TOKEN'],
-          :user_agent => 'eight&four',
-          :client_customer_id => '316-190-9175', #'529-163-1130',
-      },
+      :authentication => authentication_hash,
       :service => {
         :environment => 'PRODUCTION'
       },
