d8e50557ae3c6ccb43ff2c2cb9efd52b2caaf05e
Louise Swift <louise@louiseswift.com>
2017-02-28 12:31:22 +0000

    Strip trailing '.0's; use case statements; calculate CTR & CPC correctly

--- app/models/client_channels/facebook.rb
+++ app/models/client_channels/facebook.rb
@@ -17,7 +17,8 @@ class ClientChannels::Facebook < ClientChannel
       data_rows: fb_metrics[:data_rows].each_with_index{|data_row, index| data_row.concat(ga_metrics[:data_rows][index]) unless ga_metrics[:data_rows][index].nil? },
       summary_row: fb_metrics[:summary_row].concat(ga_metrics[:summary_row])
     }
-    return all_metrics
+    all_metrics_with_calculations = complete_calculations(all_metrics)
+    return all_metrics_with_calculations
   end
 
   private
@@ -36,8 +37,8 @@ class ClientChannels::Facebook < ClientChannel
       parsed_insights[:header_row] = AppConfig.csv_header_columns.reject{|k,v| k.include?("ga:")}.to_hash
     end
 
-    # Create data rows for each individual date within the date range searched
-    # Data rows are created from the columns in the summary table or csv report
+    # Create data rows for each individual date within the date range searched.
+    # Data rows are created from the column headers required for the summary table or csv report.
     insights_grouped_by_date = insights.group_by(&:date_start)
     insights_grouped_by_date.each do |that_days_insights|
       parsed_insights[:data_rows].push(make_row(parsed_insights[:header_row], that_days_insights))
@@ -52,46 +53,49 @@ class ClientChannels::Facebook < ClientChannel
   end
 
   def make_row(col_headers, insights)
-    # For each col_header, add the summed values or calculate the relevant data row
+    # Make a row containing the correct value to match each col_header
     row = []
     date = nil
     if valid_date?(insights.first)
       # If passed a set of insights grouped by date, the first value in 'insights'
-      # will be a date, so update insights to be the second value (the insights array)
+      # will be a date, so update 'insights' to be the 2nd value (the insights array)
       date = insights.first
       insights = insights.second
     end
 
+    # Start putting values into our row
     col_headers.each do |insight, header|
-      if insight == 'ctr'
-        average_ctr = average('clicks', 'impressions', insights)
-        row.push((average_ctr*100).round(3).to_s)
-      elsif insight == 'cpc'
-        average_cpc = average('spend', 'clicks', insights)
-        row.push(average_cpc.round(2).to_s)
-      elsif insight == 'cpm'
-        total_spend = total('spend', insights)
-        impressions_per_thousand = total('impressions', insights)/1000
-        total_spend != 0 && impressions_per_thousand != 0 ? row.push((total_spend / impressions_per_thousand).round(2).to_s) : ''
-      elsif insight == 'cpp'
-        total_spend = total('spend', insights)
-        total_reach = total('reach', insights)
-        total_spend != 0 && total_reach != 0 ? row.push((total_spend / total_reach).round(5).to_s) : ''
-      elsif insight == 'date_start' && date.present?
-        row.push(date)
-      elsif insight == 'account_name'
+      case insight
+      when 'date_start'
+        if date.present? then row.push(date) else row.push('') end
+      when 'account_name'
         row.push(self.client.name)
-      elsif insight == 'campaign_name'
+      when 'campaign_name'
         if insights.first && insights.first.campaign_id
           campaign = FacebookAds::AdCampaign.find(insights.first.campaign_id)
           row.push(campaign['name'])
         else
           row.push('')
         end
+      when 'ctr'
+        # this metric must be calculated later from ga:sessions / impressions
+        row.push('')
+      when 'cpc'
+        # this metric must be calculated later from spend / ga:sessions
+        row.push('')
+      when 'cpm'
+        total_spend = total('spend', insights)
+        impressions_per_thousand = total('impressions', insights)/1000
+        total_spend != 0 && impressions_per_thousand != 0 ? row.push((total_spend / impressions_per_thousand).round(2).to_s) : ''
+      when 'cpp'
+        total_spend = total('spend', insights)
+        total_reach = total('reach', insights)
+        total_spend != 0 && total_reach != 0 ? row.push((total_spend / total_reach).round(5).to_s) : ''
       else
         row.push(insights.sum{ |i| i[insight].to_f }.round(2).to_s)
       end
     end
+    row = strip_trailing_zeros(row)
     return row
   end
 
@@ -103,17 +107,44 @@ class ClientChannels::Facebook < ClientChannel
     !!Date.parse(value) rescue false
   end
 
-  def average(a, b, insights)
-    total_a = insights.sum{ |insight| insight[a].to_f }
-    total_b = insights.sum{ |insight| insight[b].to_f }
-    if total_a == 0 && total_b == 0
-      0
-    else
-      total_a / total_b
-    end
-  end
-
   def total(field, insights)
     insights.sum{ |insight| insight[field].to_f }
   end
+
+  def strip_trailing_zeros(row)
+    # if a value in a row ends with .0, remove that .0
+    row.map{|metric| metric.ends_with?(".0") ? metric.chomp(".0") : metric }
+  end
+
+  def complete_calculations(all_metrics)
+    # Once we have all Facebook & GA metrics together, we can calculate CTR & CPC
+    # using Facebook's impressions and spend with ga:sessions
+    all_metrics_with_calculations = all_metrics
+
+    summary_row_keypairs = all_metrics[:header_row].zip(all_metrics[:summary_row]).to_h
+    if summary_row_keypairs["CTR"] && summary_row_keypairs["Sessions"] && summary_row_keypairs["Impressions"]
+      summary_row_keypairs["CTR"] = (summary_row_keypairs["Sessions"].to_f / summary_row_keypairs["Impressions"].to_f).round(5).to_s
+    end
+    if summary_row_keypairs["CPC"] && summary_row_keypairs["Spend"] && summary_row_keypairs["Sessions"]
+      summary_row_keypairs["CPC"] = (summary_row_keypairs["Spend"].to_f / summary_row_keypairs["Sessions"].to_f).round(2).to_s
+    end
+    updated_summary_row = summary_row_keypairs.map{|k,v| v}
+    all_metrics_with_calculations[:summary_row] = updated_summary_row
+
+    all_metrics[:data_rows].each_with_index do |data_row, i|
+      data_row_keypairs = all_metrics[:header_row].zip(data_row).to_h
+      if data_row_keypairs["CTR"] && data_row_keypairs["Sessions"] && data_row_keypairs["Impressions"]
+        data_row_keypairs["CTR"] = (data_row_keypairs["Sessions"].to_f / data_row_keypairs["Impressions"].to_f).round(5).to_s
+      end
+      if data_row_keypairs["CPC"] && data_row_keypairs["Spend"] && data_row_keypairs["Sessions"]
+        data_row_keypairs["CPC"] = (data_row_keypairs["Spend"].to_f / data_row_keypairs["Sessions"].to_f).round(2).to_s
+      end
+      updated_data_row = data_row_keypairs.map{|k,v| v}
+      all_metrics_with_calculations[:data_rows][i] = updated_data_row
+    end
+
+    return all_metrics_with_calculations
+  end
+
+
 end
