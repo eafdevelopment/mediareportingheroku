71b4af75bbd12d5bd28cda299c713843a1397674
Louise Swift <louise@louiseswift.com>
2017-03-01 12:29:17 +0000

    Starting to fetch AdWords metrics

--- app/models/client_channels/adwords.rb
+++ app/models/client_channels/adwords.rb
@@ -1,16 +1,14 @@
 class ClientChannels::Adwords < ClientChannel
 
-  def fetch_metrics #(from_date, to_date, uid, campaign_name)
+  def fetch_metrics(from_date, to_date, uid, campaign_name, optional={})
     # Find and return metrics from Google AdWords
     adwords_client = get_adwords_client
-    # The following line will apparently help us programmatically set the correct
-    # client_customer_id, which is the account ID e.g. for Dirty Martini in AdWords,
-    # which needs to be set before requesting info about that account's campaigns
-    #
-    adwords_client.config.set("authentication.client_customer_id", "752-213-4824")
+    puts "> self.uid: " + self.uid.inspect
+    adwords_client.config.set("authentication.client_customer_id", self.uid) # DMU Intl: "752-213-4824"
     campaign_service = adwords_client.service(:CampaignService, :v201609)
     campaigns = campaign_service.get({:fields => ['Id', 'Name', 'Status']})
     puts "> campaigns: " + campaigns.inspect
+
   end
 
   private
@@ -18,7 +16,6 @@ class ClientChannels::Adwords < ClientChannel
   def get_adwords_client
     authentication_hash = AppConfig.adwords.merge({
       user_agent: 'eight&four'
-      # client_customer_id: '316-190-9175'
     })
 
     adwords = AdwordsApi::Api.new({
