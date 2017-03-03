219f0916d1120ceae9affe17aeceeeee5adf8b1d
Louise Swift <louise@louiseswift.com>
2017-03-03 12:02:07 +0000

    On report form submit, set 30-sec timer to undisable submit btn

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,16 +1,27 @@
 $(document).ready(function() {
 
   $('#client_select').on('change', function() {
-
     var clientId = $(this).children('option:selected').first().val();
+
     if (clientId) {
       var url = $(this).data('ajaxUrl').replace('_CLIENT_ID_', clientId);
+
       $.get(url)
         .fail(function() {
-          alert('There was a problem loading campaigns. Please refresh and re-select your client.');
+          window.alert('There was a problem loading campaigns. Please refresh and re-select your client.');
         });
     };
   });
 
   $('#client_select').trigger('change');
+
+  $('form#getReport').on('submit', function() {
+    // set a timer to un-disable the form submit button after 30 seconds
+    // (this should be replaced with a more accurate tactic at some point)
+    var $submitBtn = $(this).children('input[type="submit"]');
+    window.setTimeout(function() {
+      $submitBtn.prop('disabled', false).val('Submit');
+    }, 30000);
+  });
+
 });
