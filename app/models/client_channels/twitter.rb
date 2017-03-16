a2a888fe9ca9a192e5e22b95ce157673aab4476e
imarkwick <izzymarkwick@gmail.com>
2017-03-16 17:05:14 +0000

    time range and specifying time params to include metrics for day before

--- app/models/client_channels/twitter.rb
+++ app/models/client_channels/twitter.rb
@@ -9,50 +9,54 @@ class ClientChannels::Twitter < ClientChannel
 
   def generate_report_all_campaigns(from, to)
 
-    client = TwitterAds::Client.new(
-      ENV['TWITTER_CONSUMER_KEY'],
-      ENV['TWITTER_CONSUMER_SECRET'],
-      ENV['TWITTER_ACCESS_TOKEN'],
-      ENV['TWITTER_ACCESS_TOKEN_SECRET']
-    )
+    client = twitter_credentials
     account = client.accounts(self.uid)
 
-    from = Time.zone.now.midnight - 10.day
-    to =  Time.zone.now.midnight - 5.day
-    # from = Time.zone.parse('08-10-2016').midnight
-    # to = Time.zone.parse('11-10-2016').midnight
+    # Add a day before the search query because Twitter only accepts date
+    # params with time set at midnight, so first date isn't included in metrics
+    from = format_date(from) - 1.day
+    to = format_date(to)
+    # Remove first date from dates array as there are no metrics
+    time_array = ((from..to).map{ |date| date.strftime("%d-%m-%Y") }).drop(1)
+    api_metrics = {}
 
-    data = {}
-
-    # GETTING METRICS FROM RUBY SDK
+    #  GETTING METRICS FROM RUBY SDK
     account.line_items.each do |i|
       campaign = account.campaigns(i.campaign_id)
 
-      # Excluse Campaigns that have 'EXPIRED' or have are 'BUDGET_EXPIRED'
+      # Exclude Campaigns that have 'EXPIRED' or have are 'BUDGET_EXPIRED'
       # So as to keep API requests as low as possible
-      # Including Campaigns that are ACTIVE or 'PAUSED_BY_ADVERTISER'
+      # Include Campaigns that are ACTIVE or 'PAUSED_BY_ADVERTISER'
       if !campaign.reasons_not_servable.include?('EXPIRED') && !campaign.reasons_not_servable.include?('BUDGET_EXHAUSTED')
         puts "Getting metrics for campaign: #{campaign.name}"
         stats = TwitterAds::LineItem.stats(account, [i.id], ['ENGAGEMENT'], start_time: from, end_time: to, granularity: 'DAY')
         impressions = stats.first[:id_data].first[:metrics][:impressions]
         
         if impressions == nil
-          data[campaign.name] = nil
+          api_metrics[campaign.name] = nil
         else
-          data[campaign.name] = impressions
+          api_metrics[campaign.name] = impressions
         end
       end
     end
 
-    puts data
-
-    # Find and return Insights for a Twitter account & campaign
-    # * just dummy data for now *
-    # return {
-    #   twitterClicks: 999,
-    #   twitterCtr: 4.02002002,
-    #   twitterImpressions: 4016
-    # }
+    puts api_metrics
   end
 
+  private
+
+  def twitter_credentials
+    TwitterAds::Client.new(
+      ENV['TWITTER_CONSUMER_KEY'],
+      ENV['TWITTER_CONSUMER_SECRET'],
+      ENV['TWITTER_ACCESS_TOKEN'],
+      ENV['TWITTER_ACCESS_TOKEN_SECRET']
+    )
+  end
+
+  def format_date(date_string)
+    now = DateTime.now 
+    datetime = Time.parse(date_string)
+    return DateTime.new(datetime.year, datetime.month, datetime.day, 0, 0, 0, now.zone)
+  end
 end
