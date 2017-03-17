540a9990cdefa29debe302cb419d398c70d236d7
Louise Swift <louise@louiseswift.com>
2017-03-17 13:33:44 +0000

    Include Quality Score (avg from active adgroups) in Adwords report

--- app/models/client_channels/adwords.rb
+++ app/models/client_channels/adwords.rb
@@ -3,28 +3,31 @@ class ClientChannels::Adwords < ClientChannel
   def generate_report_all_campaigns(from_date, to_date)
     # Get data for all the campaigns for this account
     report_main = get_and_parse_report_main(from_date, to_date)
+    puts "> report_main rows: " + report_main.length.inspect
+
+    # Get a separate report containing quality score data
+    report_quality_score = get_and_parse_report_quality_score(from_date, to_date)
+
     # Get a separate report containing all conversions data
     report_conversions = get_and_parse_report_conversions(from_date, to_date)
     # And an array of all possible conversion column headers
     conversion_col_headers = get_and_parse_conversion_headers
     
     # Ready to combine main report data, conversion report data & GA data
-    header_row = AppConfig.adwords_headers.for_csv.map(&:second).concat(conversion_col_headers).concat(AppConfig.google_analytics_headers.for_csv.map(&:second))
+    header_row = AppConfig.adwords_headers.main_report.map(&:second).concat(["Quality Score (AdGroup Avg.)"]).concat(conversion_col_headers).concat(AppConfig.google_analytics_headers.for_csv.map(&:second))
     combined_data_rows = []
     report_main.each do |row|
+      associated_quality_scores = report_quality_score.select{|quality_score| quality_score["Day"] == row["Day"] && quality_score["Campaign ID"] == row["Campaign ID"]}
+      avg_quality_score = quality_score_avg_adgroup(associated_quality_scores)
       associated_conversions = report_conversions.select{|conversion| conversion["Day"] == row["Day"] && conversion["Campaign ID"] == row["Campaign ID"]}
       conversion_data_fields = create_conversion_data_fields(conversion_col_headers, associated_conversions)
       ga_data = GoogleAnalytics.fetch_and_parse_metrics(row["Day"], row["Day"], self.client.google_analytics_view_id, row["Campaign"])
       if ga_data[:data_rows].first # if GA data present, concat it
-        row = row.map{|k,v| v}.concat(conversion_data_fields).concat(ga_data[:data_rows].first)
+        row = row.map{|k,v| v}.push(avg_quality_score).concat(conversion_data_fields).concat(ga_data[:data_rows].first)
       else # if not, concat the correct number of "-"s
-        row = row.map{|k,v| v}.concat(conversion_data_fields).concat(AppConfig.google_analytics_headers.for_csv.map{|header| "-"})
+        row = row.map{|k,v| v}.push(avg_quality_score).concat(conversion_data_fields).concat(AppConfig.google_analytics_headers.for_csv.map{|header| "-"})
       end
       if row.length != header_row.length
-        puts ">> row: " + row.inspect
-        puts ">> row.length: " + row.length.inspect
-        puts ">> header_row: " + header_row.inspect
-        puts ">> header_row.length: " + header_row.length.inspect
         raise "HEADER ROW LENGTH SHOULD EQUAL FIRST DATA ROW LENGTH"
       end
       combined_data_rows.push(row)
@@ -86,7 +89,7 @@ class ClientChannels::Adwords < ClientChannel
   def get_and_parse_report_main(from_date, to_date)
     adwords_client.include_zero_impressions = true
     report_utils = adwords_client.report_utils()
-    report_main = report_utils.download_report(report_def_all_campaigns(from_date, to_date))
+    report_main = report_utils.download_report(report_def_main(from_date, to_date))
     parsed_report = CSV.parse(report_main, headers: true)
     parsed_report.each do |row|
       # Adwords API returns money in 'micro currency units' (£1 = 1,000,000)
@@ -103,6 +106,26 @@ class ClientChannels::Adwords < ClientChannel
       end
     end
     return parsed_report
+  end
+
+  def get_and_parse_report_quality_score(from_date, to_date)
+    adwords_client.include_zero_impressions = false
+    report_utils = adwords_client.report_utils()
+    report_quality_score = report_utils.download_report(report_def_criteria_performance(from_date, to_date))
+    parsed_report = []
+    CSV.parse(report_quality_score, headers: true).each do |row|
+      parsed_report.push(row.to_h)
+    end
+    return parsed_report
+  end
+
+  def quality_score_avg_adgroup(quality_score_data)
+    if quality_score_data.any?
+      # calculate avg quality score for the active adgroups found
+      return (quality_score_data.map{|data| data["Quality score"].to_i}.reduce(:+)) / quality_score_data.length
+    else
+      return "-"
+    end
   end
 
   def get_and_parse_conversion_headers
@@ -138,8 +161,8 @@ class ClientChannels::Adwords < ClientChannel
     return parsed_report
   end
 
-  def report_def_all_campaigns(from_date, to_date)
-    fields = AppConfig.adwords_headers.for_csv.map(&:first)
+  def report_def_main(from_date, to_date)
+    fields = AppConfig.adwords_headers.main_report.map(&:first)
     return {
       :selector => {
         :date_range => {
@@ -179,6 +202,35 @@ class ClientChannels::Adwords < ClientChannel
       :download_format => 'CSV',
       :report_name => 'AdWords Campaign Performance Report',
       :report_type => 'CAMPAIGN_PERFORMANCE_REPORT'
+    }
+  end
+
+  def report_def_criteria_performance(from_date, to_date)
+    fields = AppConfig.adwords_headers.criteria_performance.map(&:first)
+    return {
+      :selector => {
+        :date_range => {
+          :min => from_date.gsub(/\D/, ''),
+          :max => to_date.gsub(/\D/, '')
+        },
+        :fields => fields,
+        :predicates => [
+          {
+            :field => 'CampaignStatus',
+            :operator => 'IN',
+            :values => ['ENABLED', 'PAUSED']
+          },
+          {
+            :field => 'AdGroupStatus',
+            :operator => 'IN',
+            :values => ['ENABLED']
+          }
+        ]
+      },
+      :date_range_type => 'CUSTOM_DATE',
+      :download_format => 'CSV',
+      :report_name => 'AdWords Criteria Performance Report',
+      :report_type => 'CRITERIA_PERFORMANCE_REPORT'
     }
   end
 
