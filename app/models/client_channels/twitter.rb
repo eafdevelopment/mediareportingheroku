e4c170ba79cc7d8303ebc9dedfa636b6e5cad8f8
Louise Swift <louise@louiseswift.com>
2017-03-29 17:16:55 +0100

    Twitter batched & async requests

--- app/models/client_channels/twitter.rb
+++ app/models/client_channels/twitter.rb
@@ -14,35 +14,64 @@ class ClientChannels::Twitter < ClientChannel
     # block of stats per date to bypass 7-day limits
     data_rows = []
     campaigns = get_campaigns(self.uid)
-
-    total_requests = campaigns.count * dates_array.length
-    # plus 1 request for the account and 1 request for its campaigns
-    if total_requests > 240 # rate limit is 250
-      return { error: "This report request would exceed the API's rate limit. Please try requesting a shorter report, e.g. 7 days." }
-    end
-
+    campaign_ids = campaigns.map{|campaign| campaign[:id]}
+    data_rows = []
     begin
-      campaigns.each do |campaign|
-        puts "> getting stats for campaign: #{campaign[:name]}"
+      campaign_ids.each_slice(20) do |batched_campaign_ids|
+        puts "> batched_campaign_ids: " + batched_campaign_ids.inspect
         dates_array.each do |date|
           date_string = date.strftime('%Y-%m-%d')
-          stats = get_stats(campaign[:id], date, date+1.day)
-          impressions = begin stats.first[:id_data].first[:metrics][:impressions].first rescue nil end
-          total_spend = begin stats.first[:id_data].first[:metrics][:billed_charge_local_micro].first rescue nil end
-          billed_spend = total_spend ? (total_spend.to_f/1000000).round(2) : nil
-          ga_data = GoogleAnalytics.fetch_and_parse_metrics(date_string, date_string, self.client.google_analytics_view_id, campaign[:name])
-          data_rows.push(
-            [ date_string,
-              account.name,
-              campaign[:name],
-              campaign[:entity_status],
-              (impressions ? impressions : '-'),
-              (billed_spend ? billed_spend : '-'),
-              calculate_cpm(billed_spend, impressions)
-            ].concat(ga_data[:data_rows].first)
-          )
+          job_id = async_stats_job_id(batched_campaign_ids, date)
+          if job_id.present?
+            job_done = false
+            begin
+              stats_res = check_stats_res(job_id)
+              status = begin stats_res.body[:data].first[:status] rescue nil end
+              url = begin stats_res.body[:data].first[:url] rescue nil end
+              puts "> status: " + status.inspect
+              puts "> url: " + url.inspect
+              if status.nil? # failed request
+                job_done = nil
+              elsif url.present? # successful request
+                job_done = true
+                infile = open(url)
+                gz = Zlib::GzipReader.new(infile)
+                gz.each_line do |line|
+                  parsed_line = JSON.parse(line)
+                  all_stats = parsed_line["data"]
+                  # puts "> all_stats: " + all_stats.inspect
+                  all_stats.each do |stats|
+                    metrics = stats["id_data"].first["metrics"]
+                    puts "> metrics: " + metrics.inspect
+                    campaign_id = stats["id"]
+                    campaign = campaigns.select{|campaign| campaign[:id] == campaign_id}.first
+                    impressions = begin metrics["impressions"].first rescue nil end
+                    total_spend = begin metrics["billed_charge_local_micro"].first rescue nil end
+                    billed_spend = total_spend ? (total_spend.to_f/1000000).round(2) : nil
+                    ga_data = GoogleAnalytics.fetch_and_parse_metrics(date_string, date_string, self.client.google_analytics_view_id, campaign[:name])
+                    data_rows.push(
+                      [ date_string,
+                        account.name,
+                        campaign[:name],
+                        campaign[:entity_status],
+                        (impressions ? impressions : '-'),
+                        (billed_spend ? billed_spend : '-'),
+                        calculate_cpm(billed_spend, impressions)
+                      ].concat(ga_data[:data_rows].first)
+                    )
+                  end
+                end
+              else # request processing; try again shortly
+                sleep 2.seconds
+              end
+            end while job_done == false
+          else
+            return { error: "Could not fetch an async job ID from the Twitter API." }
+          end
         end
       end
+
+      puts "> data_rows: " + data_rows.inspect
       return { csv: to_csv(headers, data_rows) }
     
     rescue => e
@@ -77,19 +106,27 @@ class ClientChannels::Twitter < ClientChannel
       !campaign[:reasons_not_servable].include?('EXPIRED') && !campaign[:reasons_not_servable].include?('BUDGET_EXHAUSTED')
     }
   end
-  
-  def get_stats(campaign_id, from, to)
-    stats_req = TwitterAds::Request.new(twitter_credentials, :get,  "/1/stats/accounts/#{self.uid}", {params: {
-      entity: "CAMPAIGN",
-      entity_ids: campaign_id,
-      start_time:  from.strftime('%Y-%m-%dT%H:%M:%S%z'),
-      end_time:  to.strftime('%Y-%m-%dT%H:%M:%S%z'),
+
+  def async_stats_job_id(campaign_ids, date)
+    async_stats_req = TwitterAds::Request.new(twitter_credentials, :post, "/1/stats/jobs/accounts/#{self.uid}", {params: {
+      entity: 'CAMPAIGN',
+      entity_ids: campaign_ids.join(","),
+      start_time: date.strftime('%Y-%m-%d'),
+      end_time: (date + 1.day).strftime('%Y-%m-%d'),
       granularity: 'TOTAL',
       placement: 'ALL_ON_TWITTER',
       metric_groups: 'ENGAGEMENT,BILLING'
     }})
-    stats_res = stats_req.perform
-    return stats_res.body[:data]
+    async_stats_res = async_stats_req.perform
+    job_id = begin async_stats_res.body[:data][:id_str] rescue nil end
+    return job_id
+  end
+
+  def check_stats_res(job_id)
+    stats_req = TwitterAds::Request.new(twitter_credentials, :get, "/1/stats/jobs/accounts/#{self.uid}", params: {
+      job_ids: job_id
+    })
+    return stats_req.perform
   end
 
   def calculate_cpm(spend, impressions)
